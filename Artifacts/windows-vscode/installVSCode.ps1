f793f733f8149b2a47a7fd1f58c613e8df7e4c2f
Ravikanth <Ravikanth@Ravichaganti.com>
2015-11-19 18:20:28 +0530

    Adding VSCode Artifact

---
+++ Artifacts/windows-vscode/installVSCode.ps1
@@ -0,0 +1,36 @@
+﻿Function Get-RedirectedUrl {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+ 
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+$url = 'http://go.microsoft.com/fwlink/?LinkID=623230'
+$codeSetupUrl = Get-RedirectedUrl -URL $url
+
+$infPath = $PSScriptRoot + "\vscode.inf"
+
+try {
+    Invoke-WebRequest -Uri $codeSetupUrl -OutFile "${env:Temp}\VSCodeSetup.exe"
+}
+
+catch {
+    Write-Error "Failed to download VSCode Setup"
+}
+
+try {
+    Start-Process -FilePath "${env:Temp}\VSCodeSetup.exe" -ArgumentList "/VERYSILENT /LOADINF=$infPath"
+}
+
+catch {
+    Write-Error 'Failed to install VSCode'
+}
