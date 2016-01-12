b14c847d2d442a4845ae914a9403b7d3b47aacca
Ryan Vogrinec <ryanvog@microsoft.com>
2016-01-12 09:37:45 -0800

    Convert wus installer to use scheduled task

--- Artifacts/windows-wus/Install.ps1
+++ Artifacts/windows-wus/Install.ps1
@@ -1,24 +1,31 @@
 Function Get-TempPassword() {
-    Param(
-        [int]$length=10,
-        [string[]]$sourcedata
+    Param (
+        [int]       $length = 10,
+        [string[]]  $sourcedata
     )
 
-    For ($loop=1; $loop -le $length; $loop++) {
-            $tempPassword+=($sourcedata | GET-RANDOM)
+    For ($loop = 1; $loop -le $length; $loop++) {
+        $tempPassword += ($sourcedata | GET-RANDOM)
     }
 
+    Write-Host "Generated password : $tempPassword"
     return $tempPassword
 }
 
-$ascii=$NULL;For ($a=33;$a -le 126;$a++) {$ascii+=,[char][byte]$a }
+$ascii = $NULL;
+For ($a = 33; $a -le 126; $a++) {
+    if ($a -eq 34 -or $a -eq 39 -or $a -eq 96) {
+        # exclude complex symbols : ' " `
+        continue
+    }
+    $ascii+=,[char][byte]$a
+}
 
 $userName = "artifactInstaller"
 $password = Get-TempPassword -length 43 -sourcedata $ascii
 
-$cn = [ADSI]"WinNT://$env:ComputerName"
-
 # Create user
+$cn = [ADSI]"WinNT://$env:ComputerName"
 $user = $cn.Create("User", $userName)
 $user.SetPassword($password)
 $user.SetInfo()
@@ -32,26 +39,40 @@ $group.add("WinNT://$env:ComputerName/$userName")
 $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
 
-$command = $PSScriptRoot + "\UpdateWindows.ps1"
-$scriptContent = Get-Content -Path $command -Delimiter ([char]0)
-$scriptBlock = [scriptblock]::Create($scriptContent)
+Enable-PSRemoting -force -SkipNetworkProfileCheck
 
-# Run Chocolatey as the artifactInstaller user
-Enable-PSRemoting -SkipNetworkProfileCheck -Force
+$vsInstallScript = [IO.Path]::Combine($PSScriptRoot, 'UpdateWindows.ps1')
+$runProcessFile = [IO.Path]::Combine($PSScriptRoot, 'run_process_as_scheduled_task.ps1')
+Test-Path $runProcessFile
+Write-Host $runProcessFile
 
-try
+
+# Run script now
+$newProcess = new-object System.Diagnostics.ProcessStartInfo "powershell";
+$newProcess.Arguments = "-ExecutionPolicy Bypass -NoProfile -File " + $runProcessFile + " `"" + $vsInstallScript + "`" `` " + $userName + " " + $password
+Write-Host $newProcess.Arguments
+$newProcess.Verb = "runas";
+$process = [System.Diagnostics.Process]::Start($newProcess);
+$process.WaitForExit();
+Write-Host ("InstallVS: " + $process.ExitCode)
+
+
+# Cleanup
+Disable-PSRemoting -Force
+$cn.Delete("User", $userName)
+gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
+
+if ($process.ExitCode -eq 3010)
 {
-    $exitCode = Invoke-Command -ScriptBlock $scriptBlock -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList @($PSScriptRoot, "-verbose")
-}
-finally
-{
-    Disable-PSRemoting -Force
-
-    # Delete the artifactInstaller user
-    $cn.Delete("User", $userName)
-
-    # Delete the artifactInstaller user profile
-    gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
+    Write-Host ("Restarting the machine as exit code 3010 was received")
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "shutdown";
+    $newProcess.Arguments = "-r -t 0 -f"
+    Write-Host $newProcess.Arguments
+    $newProcess.Verb = "runas";
+    $process = [System.Diagnostics.Process]::Start($newProcess);
+    $process.WaitForExit();
+    $process.ExitCode
+    exit 0
 }
 
-exit $exitCode
+exit $process.ExitCode
---
+++ Artifacts/windows-wus/run_process_as_scheduled_task.ps1
@@ -0,0 +1,74 @@
+Param
+(
+  [ValidateNotNullOrEmpty()][string] $filePath,
+  [ValidateNotNullOrEmpty()][string] $arguments,
+  [ValidateNotNullOrEmpty()][string] $username,
+  [ValidateNotNullOrEmpty()][string] $password
+)
+
+$Logfile = [System.IO.Path]::Combine($PSScriptRoot, "ScheduledTaskLog" + ".txt")
+
+Add-content $Logfile -value "FilePath : $filePath"
+Add-content $Logfile -value "Arguments : $arguments"
+Add-content $Logfile -value "Username : $username"
+Add-content $Logfile -value "Password : $password"
+
+$psFilePath = [System.IO.Path]::Combine($PSScriptRoot, "Start_process_write_signal_file.ps1")
+$fileName = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
+$signalFileNameWithoutExtension = $fileName + "" + [System.Guid]::NewGuid().ToString("N").Substring(0, 5).ToLower()
+$signalFilePath = [System.IO.Path]::Combine($PSScriptRoot, $signalFileNameWithoutExtension + ".txt")
+$taskName = $signalFileNameWithoutExtension
+
+# Create a task trigger to run at user logon.
+Add-content $Logfile -value "Creating a new scheduled task"
+$taskTrigger = New-ScheduledTaskTrigger -AtLogOn
+
+# Define a task action to run the companion script.
+Add-content $Logfile -value "Creating a new scheduled task action"
+$actionExecutable = "powershell"
+$actionArgs = ("-ExecutionPolicy Bypass -NoProfile -File `"" + $psFilePath + "`" `"" + $filePath + "`" `"" + $arguments + "`" " +$signalFilePath)
+$taskAction = New-ScheduledTaskAction -Execute $actionExecutable -Argument $actionArgs
+
+# Create a new scheduled task
+Add-content $Logfile -value "Registering scheduled task : $taskName"
+$schtask = Register-ScheduledTask -TaskName $taskName -Trigger $taskTrigger -Action $taskAction -User "$env:COMPUTERNAME\$($username)" -Password $password -RunLevel Highest
+
+Add-content $Logfile -value "Starting scheduled task"
+Start-ScheduledTask -TaskName $taskName
+Start-Sleep -s 10
+Disable-ScheduledTask -TaskName $taskName
+
+# Now wait for the exit signal file
+$timeoutMinutes = 180
+$exitCode = -1
+$startTime = Get-Date
+
+# default code, means timeout
+$timeout = $false
+
+while ($timeout -eq $false)
+{
+    Add-content $Logfile -value "Checking for signal file"
+    if (Test-Path $signalFilePath)
+    {
+        $exitCode = Get-Content $signalFilePath | Out-String
+
+        Add-content $Logfile -value "Signal received, exit code = $exitCode"
+        break
+    }
+
+    Add-content $Logfile -value "Sleeping"
+    Start-Sleep -s 30
+
+    $now = Get-Date
+    $timeout = (New-TimeSpan $startTime $now).TotalMinutes -gt $timeoutMinutes
+}
+
+# set action to null
+Add-content $Logfile -value "Updating the scheduled task action"
+$emptyTaskAction = New-ScheduledTaskAction -Execute $actionExecutable
+
+Set-ScheduledTask -TaskName $taskName -Action $emptyTaskAction -User "$env:COMPUTERNAME\$($username)" -Password $password
+Add-content $Logfile -value "Exiting with exit code : $exitCode"
+
+exit $exitCode
