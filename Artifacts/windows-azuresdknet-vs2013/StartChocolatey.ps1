dbda0683d7bd5254d04cbdb213c1108d5a0f961c
Mithun Shanbhag <mithunshanbhag@hotmail.com>
2016-01-12 15:42:31 -0800

    fixes to make artifacts work with TH2 and Powershell5

--- Artifacts/windows-azuresdknet-vs2013/StartChocolatey.ps1
+++ Artifacts/windows-azuresdknet-vs2013/StartChocolatey.ps1
@@ -10,17 +10,17 @@ Function Get-TempPassword() {
         [string[]]$sourcedata
     )
 
-    For ($loop=1; $loop –le $length; $loop++) {
+    For ($loop=1; $loop -le $length; $loop++) {
             $tempPassword+=($sourcedata | GET-RANDOM)
     }
 
     return $tempPassword
 }
 
-$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
+$ascii=$NULL;For ($a=33;$a -le 126;$a++) {$ascii+=,[char][byte]$a }
 
 $userName = "artifactInstaller"
-$password = Get-TempPassword –length 43 –sourcedata $ascii
+$password = Get-TempPassword -length 43 -sourcedata $ascii
 
 $cn = [ADSI]"WinNT://$env:ComputerName"
 
@@ -43,14 +43,14 @@ $scriptContent = Get-Content -Path $command -Delimiter ([char]0)
 $scriptBlock = [scriptblock]::Create($scriptContent)
 
 # Run Chocolatey as the artifactInstaller user
-Enable-PSRemoting –force
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
 $exitCode = Invoke-Command -ScriptBlock $scriptBlock -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList @($ProductId, $PSScriptRoot)
 Disable-PSRemoting -Force
 
 # Delete the artifactInstaller user
-$cn.Delete("User", $userName)
+#$cn.Delete("User", $userName)
 
 # Delete the artifactInstaller user profile
-gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
+#gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
 
 exit $exitCode
--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Azure DevTest Lab Artifacts
-This is the public repository that contains all the currently available Azure DevTest Lab Artifacts contributed by the community. These artifacts can be deployed onto VMs in Azure DevTest Labs in the new Azure portal.
+This is the public repository that contains all the currently available Azure DevTest Lab Artifacts contributed by the community. These artifacts can be deployed onto VMs in 
+Azure DevTest Labs in the new Azure portal.
 
-Artifacts follow these guidelines - 
+## Guidelines 
 - Every artifact is contained in its own **folder** under the **artifacts** folder.
-
 - The artifact definition file is named **artifactfile.json**. The name of the artifact that shows up in Azure DevTest Labs is from the title field in the artifact definition.
-
 - The artifact folder will also include any files the artifact depends on such as icons and so on. It will optionally include  a **Readme.md** file that explains how the artifact works.
 
 We will also add scripts and samples related to Azure DevTest labs in the **scripts** and **samples** folders respectively.
+
+## Authoring
+
+See [the document 'AUTHORING' in the Documentation folder](Documentation/AUTHORING.md) for details.
 
