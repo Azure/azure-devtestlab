ac03b23431f633744d75229b66c87e3a13579ffb
jaymarf <jaymarf@microsoft.com>
2016-02-08 14:02:18 -0800

    Restored sysprepp
    artifact

--- Artifacts/windows-sysprep/Sysprep.ps1
+++ Artifacts/windows-sysprep/Sysprep.ps1
@@ -1,1 +1,6 @@
-& "$Env:SystemRoot\system32\sysprep\sysprep.exe" /generalize /oobe /shutdown 
+$newProcess = new-object System.Diagnostics.ProcessStartInfo "sysprep.exe" 
+$newProcess.WorkingDirectory = "${env:SystemDrive}\windows\system32\sysprep" 
+$newProcess.Arguments = "/generalize /oobe" 
+Write-Host $newProcess.Arguments 
+$newProcess.Verb = "runas"; 
+$process = [System.Diagnostics.Process]::Start($newProcess); 
