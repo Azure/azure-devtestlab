f2873be13c502082b5e7803be0dbb5e4585e9607
Ins4n3 <ischeglovitov@gmail.com>
2016-03-14 03:00:07 +0300

    add slack and sublime text artficacts

---
+++ Artifacts/windows-slack/startChocolatey.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+    # comma or semicolon separated list of chocolatey packages.
+    [ValidateNotNullOrEmpty()]
+    [Parameter(Mandatory=$True)]
+    [string] $packageList,
+
+    [Parameter(Mandatory=$True)]
+    [string] $username,
+
+    [Parameter(Mandatory=$True)]
+    [string] $password
+)
+
+$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
+
+$command = $PSScriptRoot + "\ChocolateyPackageInstaller.ps1"
+
+# Run Chocolatey as the specifyed user
+Enable-PSRemoting –Force -SkipNetworkProfileCheck
+
+# Ensure that current process can run scripts. 
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
+
+Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $packageList
+Disable-PSRemoting -Force
