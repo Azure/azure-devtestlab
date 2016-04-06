1f074f4a47e7f1248336127222cdb0afd222d77a
jaymarf <jaymarf@microsoft.com>
2016-04-05 23:07:22 -0700

    Update Linux Depro

--- Artifacts/linux-deprovision/depro.sh
+++ Artifacts/linux-deprovision/depro.sh
@@ -1,4 +1,17 @@
+
+
 #!/bin/bash
-set -e
-(sleep 5; waagent -force -deprovision+user) &
+OS=$(lsb_release -si)
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+VER=$(lsb_release -sr)
+if [ $VER = 15.10 ]
+        then 
+             nohup sh -c 'sleep 15 && /usr/sbin/waagent2.0 -force -deprovision+user' > /dev/null &
+        else 
+            nohup sh -c 'sleep 15 && /usr/sbin/waagent -force -deprovision+user' > /dev/null  &
+fi
+#set -e
+#nohup sh -c 'sleep 15 && /usr/sbin/waagent -force -deprovision+user' > /dev/null  &
 exit 0
+#shutdown -h +5
+
