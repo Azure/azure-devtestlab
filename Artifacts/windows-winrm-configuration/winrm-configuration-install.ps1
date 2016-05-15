08590e190ccaf182fb1d3fa7423008bf243ccf7a
Tarun <Tarun>
2016-05-15 15:58:51 +0100

    added some logging

--- Artifacts/windows-winrm-configuration/winrm-configuration-install.ps1
+++ Artifacts/windows-winrm-configuration/winrm-configuration-install.ps1
@@ -10,7 +10,7 @@
 param
 (    
     [Parameter(Mandatory = $true)]
-    [string] $hostDnsName
+    [string] $HostName
 )
 
 Set-PSDebug -Strict 
@@ -28,8 +28,6 @@ function Delete-WinRMListener
         {
             if($conf.Contains("HTTPS"))
             {
-                Write-Verbose "HTTPS is already configured. Deleting the exisiting configuration."
-    
                 winrm delete winrm/config/Listener?Address=*+Transport=HTTPS
                 break
             }
@@ -46,27 +44,31 @@ function Configure-WinRMHttpsListener
     param([string] $HostName,
           [string] $port)
 
-    # Delete the WinRM Https listener if it is already configured
+    #Delete the WinRM Https listener if it is already configure
     Delete-WinRMListener
-
+    
     # Create a test certificate
-    $thumbprint = (Get-ChildItem cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
+    $thumbprint = (Get-ChildItem cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=" + $HostName } | Select-Object -Last 1).Thumbprint
     if(-not $thumbprint)
     {
-	# makecert ocassionally produces negative serial numbers
+    # makecert ocassionally produces negative serial numbers
 	# which golang tls/crypto <1.6.1 cannot handle
 	# https://github.com/golang/go/issues/8265
         $serial = Get-Random
-        .\makecert -r -pe -n CN=$hostname -b 01/01/2012 -e 01/01/2022 -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -# $serial
-        $thumbprint=(Get-ChildItem cert:\Localmachine\my | Where-Object { $_.Subject -eq "CN=" + $hostname } | Select-Object -Last 1).Thumbprint
-
-        if(-not $thumbprint)
+        .\makecert -r -pe -n CN=$Hostname -b 01/01/2012 -e 01/01/2022 -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -# $serial
+        $thumbprint=(Get-ChildItem cert:\Localmachine\my | Where-Object { $_.Subject -eq "CN=" + $HostName } | Select-Object -Last 1).Thumbprint
+        
+		if(-not $thumbprint)
         {
             throw "Failed to create the test certificate."
         }
     }    
 
-    $response = cmd.exe /c .\winrmconf.cmd $hostname $thumbprint
+    #$response = cmd.exe /c $currentLocation\winrmconf.cmd $HostName $thumbprint
+	$resonse = cmd.exe .\winrmconf.cmd $hostname $thumbprint
+	
+    Write-Host "The response is... "
+    Write-Host $response
 }
 
 function Add-FirewallException
@@ -76,19 +78,19 @@ function Add-FirewallException
     # Delete an exisitng rule
     netsh advfirewall firewall delete rule name="Windows Remote Management (HTTPS-In)" dir=in protocol=TCP localport=$port
 
-    # Add a new firewall rule
+	# Add a new firewall rule
     netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=$port
 }
-
 
 #################################################################################################################################
 #                                              Configure WinRM                                                                  #
 #################################################################################################################################
 
 $winrmHttpsPort=5986
+$currentLocation=Split-Path -parent $MyInvocation.MyCommand.Definition
 
 # Configure https listener
-Configure-WinRMHttpsListener $HostName $winrmHttpsPort
+Configure-WinRMHttpsListener $HostName
 
 # Add firewall exception
 Add-FirewallException -port $winrmHttpsPort
