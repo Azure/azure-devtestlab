1804adba0de4630c3fe23186c7feee3bc5dbd119
Mark Mydland <markmyd@microsoft.com>
2016-09-09 18:14:35 -0700

    use try-catch to get make the script return -1

--- Artifacts/windows-vsts-download-and-run-script/DownloadVstsDropAndExecuteScript.ps1
+++ Artifacts/windows-vsts-download-and-run-script/DownloadVstsDropAndExecuteScript.ps1
@@ -41,8 +41,21 @@ function SetAuthHeaders
 function GetBuildDefinitionId
 {
     $buildDefinitionUri = ("{0}/_apis/build/definitions?api-version={1}&name={2}" -f $vstsProjectUri, $vstsApiVersion, $buildDefinitionName)
-    $buildDef = Invoke-RestMethod -Uri $buildDefinitionUri -Headers $headers -method Get
-    return $buildDef.value.id
+    try
+    {
+        $buildDef = Invoke-RestMethod -Uri $buildDefinitionUri -Headers $headers -method Get
+        return $buildDef.value.id
+    }
+    catch
+    {
+        if (($null -ne $Error[0]) -and ($null -ne $Error[0].Exception) -and ($null -ne $Error[0].Exception.Message))
+        {
+            $errMsg = $Error[0].Exception.Message
+            WriteLog $errMsg
+            Write-Host $errMsg
+        }
+        exit -1
+    }
 }
 
 function GetLatestBuild
@@ -52,8 +65,23 @@ function GetLatestBuild
         [int] $buildDefinitionId 
     )
     $buildUri = ("{0}/_apis/build/builds?api-version={1}&definitions={2}&resultFilter=succeeded" -f $vstsProjectUri, $vstsApiVersion, $buildDefinitionId);
-    $builds = Invoke-RestMethod -Uri $buildUri -Headers $headers -Method Get | ConvertTo-Json | ConvertFrom-Json
-    return $builds.value[0].id
+
+    try 
+    {
+        $builds = Invoke-RestMethod -Uri $buildUri -Headers $headers -Method Get | ConvertTo-Json | ConvertFrom-Json
+        return $builds.value[0].id
+    }
+    catch
+    {
+        if (($null -ne $Error[0]) -and ($null -ne $Error[0].Exception) -and ($null -ne $Error[0].Exception.Message))
+        {
+            $errMsg = $Error[0].Exception.Message
+            WriteLog $errMsg
+            Write-Host $errMsg
+        }
+        exit -1
+    }
+   
 }
 
 function DownloadBuildArtifacts
@@ -61,18 +89,32 @@ function DownloadBuildArtifacts
     $headers = SetAuthHeaders
     $buildId = GetLatestBuild ( GetBuildDefinitionId )
     $artifactsUri = ("{0}/_apis/build/builds/{1}/Artifacts?api-version={2}" -f $vstsProjectUri, $buildId, $vstsApiVersion);
-    $artifacts = Invoke-RestMethod -Uri $artifactsUri -Headers $headers -Method Get | ConvertTo-Json -Depth 3 | ConvertFrom-Json
-    $DownloadUri = $artifacts.value.resource.downloadUrl
 
-    Invoke-RestMethod -Uri $DownloadUri -Headers $headers -Method Get -Outfile $outfile 
-
-    if (Test-Path $destination -PathType Container)
+    try 
     {
-        Remove-Item -Path $destination -Force -Recurse -Verbose
-    }
+        $artifacts = Invoke-RestMethod -Uri $artifactsUri -Headers $headers -Method Get | ConvertTo-Json -Depth 3 | ConvertFrom-Json
+        $DownloadUri = $artifacts.value.resource.downloadUrl
 
-    [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null 
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($outfile, $destination)
+        Invoke-RestMethod -Uri $DownloadUri -Headers $headers -Method Get -Outfile $outfile 
+
+        if (Test-Path $destination -PathType Container)
+        {
+            Remove-Item -Path $destination -Force -Recurse -Verbose
+        }
+
+        [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null 
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($outfile, $destination)
+    }
+    catch
+    {
+        if (($null -ne $Error[0]) -and ($null -ne $Error[0].Exception) -and ($null -ne $Error[0].Exception.Message))
+        {
+            $errMsg = $Error[0].Exception.Message
+            WriteLog $errMsg
+            Write-Host $errMsg
+        }
+        exit -1
+    }
 }
  
 function RunScript
