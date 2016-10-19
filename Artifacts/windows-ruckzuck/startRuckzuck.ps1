9ec5960b729b618b3d7ee89343f2468d3a85f891
Roger Zander <roger@zander.ch>
2016-10-19 11:21:47 +0200

    windows-RuckZuck artifact added

---
+++ Artifacts/windows-ruckzuck/startRuckzuck.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()]
+param(
+    # comma- or semicolon-separated list of RuckZuck packages.
+    [string] $PackageList
+)
+
+	
+cd $($PSScriptRoot)
+	
+if([string]::IsNullOrEmpty($PackageList))
+{
+    Start-Process "RZUpdate.exe" -ArgumentList "/Update"
+}
+else 
+    {
+        foreach($pkg in $PackageList.Split(",;")) 
+        {
+		    try
+		    {
+			    $proc = (Start-Process -FilePath "RZUpdate.exe" -ArgumentList "$($pkg)");$proc.WaitForExit();$ExitCode = $proc.ExitCode
+		    } 
+            catch{}
+	}
+}
