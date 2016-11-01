fbfa27117971c3cc877b43dfa5b285dce1a03c55
Roger Zander <roger@zander.ch>
2016-11-01 20:20:02 +0100

    Removed RZUpdate.exe from Repository

--- Artifacts/windows-ruckzuck/startRuckzuck.ps1
+++ Artifacts/windows-ruckzuck/startRuckzuck.ps1
@@ -6,12 +6,17 @@ param(
 	
 cd $($PSScriptRoot)
 
-#RZUpdate.exe V1.1.0.4 from https://ruckzuck.codeplex.com/
+if((Test-Path "$($env:temp)\RZUpdate.exe") -eq $false) 
+{ 
+	(New-Object System.Net.WebClient).DownloadFile("https://ruckzuck.azurewebsites.net/DL/RZUpdate.exe", "$($env:temp)\RZUpdate.exe") 
+}
+
+#RZUpdate.exe from https://ruckzuck.codeplex.com/
 #Check if Package-List is empty..	
 if([string]::IsNullOrEmpty($PackageList))
 {
 	#Update all existing Software
-    $proc = (Start-Process -FilePath "RZUpdate.exe" -ArgumentList "/Update")
+    $proc = (Start-Process -FilePath "$($env:temp)\RZUpdate.exe" -ArgumentList "/Update")
 	$proc.WaitForExit()
 	$ExitCode = $proc.ExitCode
     Exit($ExitCode)
@@ -21,7 +26,7 @@ else
 	#Install all Products from the Package-List
 	try
 	{
-	    $proc = (Start-Process -FilePath "RZUpdate.exe" -ArgumentList "$($PackageList)")
+	    $proc = (Start-Process -FilePath "$($env:temp)\RZUpdate.exe" -ArgumentList "$($PackageList)")
 		$proc.WaitForExit()
 		$ExitCode = $proc.ExitCode
 	} 
