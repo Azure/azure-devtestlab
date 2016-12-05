ad92a78cf1d35a794638c965a84d50aa6979d4c5
joerage <jonathan_rajotte@hotmail.com>
2016-12-05 17:21:26 -0500

    Revert "Downloads the latest Docker installer script and runs it."

--- Artifacts/linux-install-docker-container/linux_install_docker_container.sh
+++ Artifacts/linux-install-docker-container/linux_install_docker_container.sh
@@ -12,7 +12,20 @@ set -e
 #
 ###
 
-wget -qO- https://get.docker.com/ | sh
+if [ -f /usr/bin/apt ] ; then
+    echo "Using APT package manager"
+
+    apt-get -y update
+    apt-get -y install docker.io
+elif [ -f /usr/bin/yum ] ; then 
+    echo "Using YUM package manager"
+
+    yum -y update
+    yum install -y docker
+
+    systemctl start docker
+    systemctl enable docker
+fi
 
 if [ -z "$1" ] ; then
     NAME_ARG=
