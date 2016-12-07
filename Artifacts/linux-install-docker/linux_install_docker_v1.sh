4cfac48633ae9491ae5ec72a4fcac6078d56c084
Jonathan Rajotte <jraj@microsoft.com>
2016-12-07 15:48:53 -0500

    a

--- Artifacts/linux-install-docker/linux_install_docker_v1.sh
+++ Artifacts/linux-install-docker/linux_install_docker_v1.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-set -e
-
-# Check if docker is already installed.
-docker -v
-installationStatus=$(echo $?)
-
-if [ $installationStatus -eq 127 ] ; then
-    wget -qO- https://get.docker.com/ | sh
-fi
+exit 1
