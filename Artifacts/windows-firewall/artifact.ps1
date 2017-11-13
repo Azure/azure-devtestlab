87fa16ca1bb4b47679db00f99fe1f2641419981a
Milan Musec <milan.musec@shell.com>
2017-11-13 09:19:28 +0000

    windows-firewall artifact added new operations

--- Artifacts/windows-firewall/artifact.ps1
+++ Artifacts/windows-firewall/artifact.ps1
@@ -17,6 +17,15 @@ switch ($firewallOperation) {
         & netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
         & netsh advfirewall firewall add rule name="ICMP Allow incoming V6 echo request" protocol=icmpv6:8,any dir=in action=allow
     }
+    "Enable RDP Port (tcp/3389)" {
+        & netsh advfirewall firewall add rule name="Allow RDP Connections from anywhere" protocol=tcp localport=3389 remoteip=any dir=in action=allow
+    }
+    "Enable SSH Port (tcp/22)" {
+        & netsh advfirewall firewall add rule name="Allow SSH Connections from anywhere" protocol=tcp localport=22 remoteip=any dir=in action=allow
+    }
+    "Enable HTTP/HTTPS Port (tcp/80,443)" {
+        & netsh advfirewall firewall add rule name="Allow HTTP/HTTPS Connections from anywhere" protocol=tcp localport=80,443 remoteip=any dir=in action=allow
+    }
     "Enable Firewall" {
         # PS commands work on ly on powershel 5 and higher
         # Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
