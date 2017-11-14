8c9beac3144e78668f09a32fb2727050a6e9cb48
Milan Musec <milan.musec@shell.com>
2017-11-14 19:28:07 +0000

    implemented comments

--- Artifacts/windows-firewall/artifact-main.ps1
+++ Artifacts/windows-firewall/artifact-main.ps1
@@ -17,7 +17,24 @@ pushd $PSScriptRoot
 # Functions used in this script.
 #
 
-."./artifact-funcs.ps1"
+function Handle-LastError
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
 
 ###################################################################################################
 
--- Artifacts/windows-firewall/artifact.ps1
+++ Artifacts/windows-firewall/artifact.ps1
@@ -4,16 +4,13 @@ param
     [Parameter(Mandatory=$true)]
     [string] $firewallOperation
 )
-Write-Output ("Powershell "+$PSVersionTable.PSVersion.Major.ToString()+"."+$PSVersionTable.PSVersion.Minor.ToString()+"."+$PSVersionTable.PSVersion.Build.ToString()+"."+$PSVersionTable.PSVersion.Revision.ToString())
+Write-Output "PowerShell $($PSVersionTable.PSVersion)"
 Write-Output "Selected operation: $firewallOperation"
 switch ($firewallOperation) {
-    "Firewall Status" {
+    "Get Firewall Status" {
         & netsh advfirewall show allprofiles
     }    
     "Enable ICMP (ping)" {  
-        # PS commands work on ly on powershel 5 and higher
-        # New-NetFirewallRule -DisplayName "Allow inbound ICMPv4" -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -RemoteAddress any -Action Allow
-        # New-NetFirewallRule -DisplayName "Allow inbound ICMPv6" -Direction Inbound -Protocol ICMPv6 -IcmpType 8 -RemoteAddress any -Action Allow        
         & netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
         & netsh advfirewall firewall add rule name="ICMP Allow incoming V6 echo request" protocol=icmpv6:8,any dir=in action=allow
     }
@@ -27,13 +24,15 @@ switch ($firewallOperation) {
         & netsh advfirewall firewall add rule name="Allow HTTP/HTTPS Connections from anywhere" protocol=tcp localport=80,443 remoteip=any dir=in action=allow
     }
     "Enable Firewall" {
-        # PS commands work on ly on powershel 5 and higher
-        # Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
         & netsh advfirewall set allprofiles state on 
     }
     "Disable Firewall" {
-        #Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
         & netsh advfirewall set allprofiles state off
     }
     Default { Write-Output "No operation executed"}
+
+}
+if ($LASTEXITCODE -ne 0)
+{
+    throw 'The artifact failed to apply.'
 }
