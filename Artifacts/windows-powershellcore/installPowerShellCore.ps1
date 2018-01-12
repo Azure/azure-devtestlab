3242ed4d35a44e76fbbe80b670169a25e45f9e22
Chaganti <ravikanth_chaganti@amer.dell.com>
2018-01-12 09:51:07 +0530

    Adding PowerShell Core DTL artifact for windows
    This artifact installs PowerShell Core on Windows OS

---
+++ Artifacts/windows-powershellcore/installPowerShellCore.ps1
@@ -0,0 +1,26 @@
+﻿param
+(
+    [Parameter(Mandatory)]
+    [String]
+    $Url
+)
+
+if (-not (Split-Path -Path $url -Leaf).EndsWith('.msi'))
+{
+    throw "${Url} is not the path to PowerShell Core MSI."
+}
+
+try
+{
+    $coreMSI = "${env:Temp}\PowerShellCore.msi"
+    (New-Object System.Net.WebClient).DownloadFile($Url, $coreMSI)
+    $msiProcess = Start-Process -FilePath msiexe.exe -ArgumentList "/i ${coreMSI} /quiet /qn" -Wait -PassThru
+    if (-not $msiProcess.ExitCode -eq 0)
+    {
+        Write-Error -Message 'Failed to install PowerShell Core'
+    }
+}
+catch
+{
+    Write-Error -Message $_
+}
