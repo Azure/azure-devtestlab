6d31cc72a54a2c19468e9596bf82b18ca418cdfd
leovms <leov@microsoft.com>
2018-04-09 12:50:29 -0400

    Updated method used to install chocolatey (#366)

--- Artifacts/windows-chocolatey/install-choco-package.ps1
+++ Artifacts/windows-chocolatey/install-choco-package.ps1
@@ -60,11 +60,16 @@ function Ensure-Chocolatey
 {
     [CmdletBinding()]
     param(
+        [string] $ChocoExePath
     )
 
-    if (-not (Test-Path "$choco"))
+    if (-not (Test-Path "$ChocoExePath"))
     {
-        Invoke-ExpressionImpl -Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) | Out-Null
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        if ($LastExitCode -eq 3010)
+        {
+            Write-Host 'The recent changes indicate a reboot is necessary. Please reboot at your earliest convenience.'
+        }
     }
 }
 
@@ -85,11 +90,12 @@ function Install-Packages
 {
     [CmdletBinding()]
     param(
+        [string] $ChocoExePath,
         $Packages
     )
 
     $Packages = $Packages.split(',; ', [StringSplitOptions]::RemoveEmptyEntries) -join ' '
-    $expression = "$choco install -y -f --acceptlicense --allow-empty-checksums --no-progress --stoponfirstfailure $Packages"
+    $expression = "$ChocoExePath install -y -f --acceptlicense --allow-empty-checksums --no-progress --stoponfirstfailure $Packages"
     Invoke-ExpressionImpl -Expression $expression 
 }
 
@@ -120,7 +126,7 @@ function Invoke-ExpressionImpl
         }
         else
         {
-            throw 'Installation failed. Please see the Chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details.'
+            throw "Installation failed ($LastExitCode). Please see the Chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details."
         }
     }
 }
@@ -154,10 +160,10 @@ try
     Enable-PSRemoting -Force -SkipNetworkProfileCheck | Out-Null
 
     Write-Host 'Ensuring latest Chocolatey version is installed.'
-    Ensure-Chocolatey
+    Ensure-Chocolatey -ChocoExePath "$choco"
 
     Write-Host "Preparing to install Chocolatey packages: $Packages."
-    Install-Packages -Packages $Packages
+    Install-Packages -ChocoExePath "$choco" -Packages $Packages
 
     Write-Host "`nThe artifact was applied successfully.`n"
 }
