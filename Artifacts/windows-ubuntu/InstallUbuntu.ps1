60806698f23554f0d4d607820951f3c16a556440
steve kurtz <steve.kurtz@microsoft.com>
2018-10-14 18:05:57 -0400

    Added windows-wsl and windows-ubuntu
    WIndows-wsl enables windows subsystem for Linux, and then reboots. Windows-ubuntu installs the specific Ubuntu distro.

---
+++ Artifacts/windows-ubuntu/InstallUbuntu.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string] $distro
+)
+
+Write-Output "Creating Ubuntu Distro Folder"
+    $sysDrive = $env:SystemRoot.Substring(0,3)
+    $ubuntuPath=New-Item -ItemType Directory -Force -Path $sysDrive\Distros\
+    Set-Location $ubuntuPath
+
+Write-Output "Downloading Ubuntu Distro"    
+    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-$distro -OutFile Ubuntu.appx -UseBasicParsing
+
+Write-Output "Renaming .appx to .zip"    
+    Rename-Item $ubuntuPath/Ubuntu.appx $ubuntuPath/Ubuntu.zip
+
+Write-Output "Unzipping Distro"    
+    Expand-Archive $ubuntuPath/Ubuntu.zip $ubuntuPath/Ubuntu
+
+Write-Output "Installing Ubuntu"      
+    $installerPath=Get-ChildItem -Path $ubuntuPath/Ubuntu -include ubuntu* | where {$_.Extension -eq ".exe"}
+    "$installerPath install --root" | cmd
