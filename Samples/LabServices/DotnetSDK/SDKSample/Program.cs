b3f179fa41a9133c9ff95118caf4b74668227e84
Ian Hays <ianha@microsoft.com>
2019-04-04 14:27:29 -0700

    Add samples for Lab Services dotnet SDK (#456)
    * Add samples for Lab Services dotnet SDK
    * Convert labservices instructions to md file

---
+++ Samples/LabServices/DotnetSDK/SDKSample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SDKSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Missing required parameters.");
+                    return;
+                }
+
+                switch (args[0])
+                {
+                    case "getAllRdpFilesForLab":
+                        {
+                            GetRdpFilesForLab.CreateRdpFiles().Wait();
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Saved all RDP files!");
+                            Console.ResetColor();
+
+                            break;
+                        }
+                    case "getAllRdpFilesForUser":
+                        {
+                            GetRdpFilesForUser.CreateRdpFiles().Wait();
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Saved all RDP files!");
+                            Console.ResetColor();
+
+                            break;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ResetColor();
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press <ENTER> to continue.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
