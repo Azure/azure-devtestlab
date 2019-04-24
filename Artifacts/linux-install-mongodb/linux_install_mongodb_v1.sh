529326ff4035414407c128292836557de6a191cb
leovms <14631204+leovms@users.noreply.github.com>
2019-04-24 18:23:08 -0400

    Fixed hang when installing MongoDB on RHEL
    * Adding more logging.
    * Adding command to clean yum cache.
    * Disabling faulty packaged (i.e. rhui-microsoft-azure-rhel7,microsoft-azure-rhel7)
    * Removing service start command, which may hang because of a lock while installing.
    * Configuring artifact to restart the VM.

--- Artifacts/linux-install-mongodb/linux_install_mongodb_v1.sh
+++ Artifacts/linux-install-mongodb/linux_install_mongodb_v1.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-echo "Installing MongoDb"
+echo "Starting MongoDb installation."
 
 isApt=`command -v apt-get`
 isYum=`command -v yum`
 
 # Some of the previous commands will fail with an exit code other than zero (intentionally), 
-# so we do not set error handling to stop (set e) until after they've run
+# so we do not set error handling to stop (set e) until after they've run.
 set -e
 
 if [ -n "$isApt" ] ; then
-    echo "Using APT package manager"
+    echo "Using APT package manager."
     
+    echo "Updating every installed package."
     apt-get -y update
+
+    echo "Installing MongoDb."
     apt-get -y install mongodb
 elif [ -n "$isYum" ] ; then
-    echo "Using YUM package manager"
+    echo "Using YUM package manager."
 
+    echo "Cleaning everything."
+    rm -r /var/cache/yum -f
     yum clean all
-    yum -y update
-    
+    echo "Updating every installed package."
+    yum -y update --disablerepo=rhui-microsoft-azure-rhel7,microsoft-azure-rhel7
+
+    echo "Preparing MongoDb installation configuration."
     releasever=7
     cat >/etc/yum.repos.d/mongodb-org-3.0.repo <<-EOF
 [mongodb-org-3.0]
@@ -28,9 +35,9 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/
 gpgcheck=0
 enabled=1
 EOF
-    
-    yum install -y mongodb-org
-    
-    service mongod start
-    chkconfig mongod on
+
+    echo "Installing MongoDb."
+    yum install -y mongodb-org --disablerepo=rhui-microsoft-azure-rhel7,microsoft-azure-rhel7
 fi
+
+echo "Artifact completed successfully."
