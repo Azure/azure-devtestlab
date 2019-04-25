8fc15abf9d83ea091fa6979fd40d2b50f7909416
leovms <14631204+leovms@users.noreply.github.com>
2019-04-25 15:37:10 -0400

    Changing method used to allocate swap files (#477)
    In previous versions of this script, we used fallocate. But that started failing on newer OS versions. So, although fallocate can be faster, we are changing to use dd instead. The nomenclature is slightly different. So, we need to make sure the user is aware of it. For example, to create a 1 GB swap file, the caller will need to specify 1073741824 (bytes) or 1G. Note that case here is important.

--- Artifacts/linux-swapfile/linux_swapfile.sh
+++ Artifacts/linux-swapfile/linux_swapfile.sh
@@ -9,13 +9,20 @@
 
 set -e
 
-# Generate a file name based on the current date to avoid naming conflicts
+# Generate a file name based on the current date to avoid naming conflicts.
 SIZE="$1"
 FILENAME="$(date +%s | sha256sum | base64 | head -c 32)"
 FILEPATH="/mnt/$FILENAME"
 
-fallocate -l $SIZE $FILEPATH
+# In previous versions of this script, we used fallocate. But that started failing on newer
+# OS versions. So, although fallocate can be faster, we are changing to use dd instead. The
+# nomenclature is slightly different. So, we need to make sure the user is aware of it.
+# For example, to create a 1 GB swap file, the caller will need to specify 1073741824 (bytes)
+# or 1G. Note that case here is important.
+dd if=/dev/zero of=$FILEPATH bs=$SIZE count=1
+
 chmod 600 $FILEPATH
 mkswap $FILEPATH
 swapon $FILEPATH
+
 echo “$FILEPATH  none  swap  sw  0 0” >> /etc/fstab
