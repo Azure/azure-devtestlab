ae6cedf7c5e384b4f79d30ba218130d0d96a9c3c
Peter Hauge <phauge@microsoft.com>
2019-06-14 09:41:04 -0400

    Updating scenario tests for correct powershell syntax

--- samples/DevTestLabs/Modules/Library/Scenarios/ScenarioAllFeatures.ps1
+++ samples/DevTestLabs/Modules/Library/Scenarios/ScenarioAllFeatures.ps1
@@ -27,41 +27,41 @@ $lab2, $rgName
 $labs = $labsData `
         | StringToFile `
         | Import-Csv `
-        | Dtl-NewLab -AsJob `
+        | New-AzDtlLab -AsJob `
         | Receive-Job -Wait
 
 $labs `
-  | Dtl-AddUser -UserEmail 'lucabol@microsoft.com' `
-  | Dtl-SetLabAnnouncement -Title 'I am here' -AnnouncementMarkDown 'yep' `
-  | Dtl-SetLabSupport -SupportMarkdown "### Sample lab announcement header." `
-  | Dtl-SetLabRdp -GatewayUrl 'Agtway@adomain.com' -ExperienceLevel 5 `
-  | Dtl-SetLabShutdown -ShutdownTime '21:00' -TimeZoneId 'UTC' -ScheduleStatus 'Enabled' -NotificationSettings 'Enabled' `
+  | Add-AzDtlLabUser -UserEmail 'lucabol@microsoft.com' `
+  | Set-AzDtlLabAnnouncement -Title 'I am here' -AnnouncementMarkDown 'yep' `
+  | Set-AzDtlLabSupport -SupportMarkdown "### Sample lab announcement header." `
+  | Set-AzDtlLabRdpSettings -GatewayUrl 'Agtway@adomain.com' -ExperienceLevel 5 `
+  | Set-AzDtlLabShutdown -ShutdownTime '21:00' -TimeZoneId 'UTC' -ScheduleStatus 'Enabled' -NotificationSettings 'Enabled' `
       -TimeInIMinutes 50 -ShutdownNotificationUrl 'https://blah.com' -EmailRecipient 'blah@lab.com' `
-  | Dtl-SetLabStartup -StartupTime '21:00' -TimeZoneId 'UTC' -WeekDays @('Monday') `
-  | Dtl-AddLabRepo -ArtifactRepoUri 'https://github.com/lucabol/DTLWorkshop.git' `
+  | Set-AzDtlLabStartupSchedule -StartupTime '21:00' -TimeZoneId 'UTC' -WeekDays @('Monday') `
+  | Add-AzDtlLabArtifactRepository -ArtifactRepoUri 'https://github.com/lucabol/DTLWorkshop.git' `
       -artifactRepoSecurityToken '196ad1f5b5464de4de6d47705bbcab0ce7d323fe' `
-  | Dtl-NewVm -VmName ("vm" + (Get-Random)) -Size 'Standard_A4_v2' -Claimable -UserName 'bob' -Password 'aPassword341341' `
+  | New-AzDtlVm -VmName ("vm" + (Get-Random)) -Size 'Standard_A4_v2' -Claimable -UserName 'bob' -Password 'aPassword341341' `
       -OsType Windows -Sku '2012-R2-Datacenter' -Publisher 'MicrosoftWindowsServer' -Offer 'WindowsServer' `
       -AsJob `
   | Receive-Job -Wait `
-  | Dtl-StartVm `
-  | Dtl-ApplyArtifact -RepositoryName 'Public Artifact Repo' -ArtifactName 'windows-7zip' `
-  | Dtl-SetAutoStart `
-  | Dtl-SetVmShutdown -ShutdownTime '20:00' -TimeZoneId 'UTC' `
-  | Dtl-ClaimVm `
-  | Dtl-StopVm `
-  | Dtl-RemoveVm
+  | Start-AzDtlVm `
+  | Set-AzDtlVmArtifact -RepositoryName 'Public Artifact Repo' -ArtifactName 'windows-7zip' `
+  | Set-AzDtlVmAutoStart `
+  | Set-AzDtlVmShutdownSchedule -ShutdownTime '20:00' -TimeZoneId 'UTC' `
+  | Invoke-AzDtlVmClaim `
+  | Stop-AzDtlVm `
+  | Remove-AzDtlVm
 
-$labs | Dtl-GetLabSchedule -ScheduleType 'AutoShutdown' | Out-Null
+$labs | Get-AzDtlLabSchedule -ScheduleType 'AutoShutdown' | Out-Null
 
 $customImage = $labs[0] `
-  | Dtl-NewVm -VmName ("cvm" + (Get-Random)) -Size 'Standard_A4_v2' -Claimable -UserName 'bob' -Password 'aPassword341341' `
+  | New-AzDtlVm -VmName ("cvm" + (Get-Random)) -Size 'Standard_A4_v2' -Claimable -UserName 'bob' -Password 'aPassword341341' `
     -OsType Windows -Sku '2012-R2-Datacenter' -Publisher 'MicrosoftWindowsServer' -Offer 'WindowsServer' `
-  | Dtl-NewCustomImage -ImageName ("im" + (Get-Random)) -ImageDescription 'Created using Azure DevTest Labs PowerShell library.'
+  | New-AzDtlCustomImageFromVm -ImageName ("im" + (Get-Random)) -ImageDescription 'Created using Azure DevTest Labs PowerShell library.'
 
-$labs[0] | Dtl-NewVm -CustomImage $customImage -VmName ('cvm2' + (Get-Random)) -Size 'Standard_A4_v2' -OsType Windows | Out-Null
+$labs[0] | New-AzDtlVm -CustomImage $customImage -VmName ('cvm2' + (Get-Random)) -Size 'Standard_A4_v2' -OsType Windows | Out-Null
 
-$labs | Dtl-RemoveLab
+$labs | Remove-AzDtlLab
 Remove-AzureRmResourceGroup -Name $rgName -Force | Out-Null
 
 Remove-Module Az.DevTestLabs2 -Force -Verbose:$false
