2c0b6c22c06d75fe0a89645d5c56897fe6d98888
Peter Hauge <phauge@microsoft.com>
2019-07-09 11:57:37 -0400

    C# and Powershell samples on keeping Internal Support DTL page up to date with Azure Functions

---
+++ samples/DevTestLabs/AzureFunctions/CSharp/ApplyWindowsUpdateArtifact.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Management.DevTestLabs;
+using Microsoft.Azure.Management.DevTestLabs.Models;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace AzureFunctions
+{
+    public static class ApplyWindowsUpdateArtifact
+    {
+        [FunctionName("ApplyWindowsUpdateArtifact")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post",
+            Route = "subscriptions/{SUBSCRIPTIONID}/resourceGroups/{RESOURCEGROUPNAME}/providers/Microsoft.DevTestLab/labs/{LABNAME}/virtualmachines/{VIRTUALMACHINENAME}")] HttpRequest req,
+            string subscriptionId,
+            string resourceGroupName,
+            string labName,
+            string virtualMachineName,
+            ILogger log)
+        {
+            log.LogInformation($"Function to apply the Windows Update artifact on VM {virtualMachineName} in lab {labName} in subscription {subscriptionId} started processing at {DateTime.Now}");
+
+            // Authentication helper, keeps track of the access token and encapsulates authentication into Azure & DTL Client
+            var authenticationHelper = new Authentication(log);
+            await authenticationHelper.RetrieveAccessTokenAsync();
+
+            // login to the DevTest Labs APIs
+            var dtlClient = authenticationHelper.LoginToDevTestLabsAPIs(subscriptionId);
+
+            string labResourceId = string.Empty;
+            try
+            {
+                // this call throws an exception if we can't find the lab.  The most likely case 
+                // is that the request contained invalid information (invalid lab name for example)
+                var lab = await dtlClient.Labs.GetAsync(resourceGroupName, labName);
+                labResourceId = lab.Id;
+            }
+            catch (Exception e)
+            {
+                log.LogError($"{e.Message}  Perhaps the ResourceGroupName '{resourceGroupName}' or LabName '{labName}' doesn't exist?");
+                throw;
+            }
+
+            // Get the virtual machine, let's make sure it's running
+            LabVirtualMachine vm = null;
+            try
+            {
+                // this call throws an exception if we can't find the VM.  Most likely case
+                // is that the request contained invalid info (invalid lab name, or invalid VM name)
+                vm = await dtlClient.VirtualMachines.GetAsync(resourceGroupName, labName, virtualMachineName);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"{e.Message}  Perhaps the ResourceGroupName '{resourceGroupName}' or LabName '{labName}' or VM '{virtualMachineName}' doesn't exist?");
+                throw;
+            }
+
+            if (vm.LastKnownPowerState == "Running")
+            {
+                var artifactRequest = new ApplyArtifactsRequest(new List<ArtifactInstallProperties> {
+                    new ArtifactInstallProperties($"{labResourceId}/artifactSources/privaterepo719/artifacts/windows-installwindowsupdates")
+                });
+
+                // We fire off the request to apply artifacts, but we don't wait until it's complete before wrapping up the function
+                // If we wanted to wait, we would use "dtlClient.VirtualMachines.ApplyArtifactsAsync()"
+                dtlClient.VirtualMachines.BeginApplyArtifacts(resourceGroupName, labName, virtualMachineName, artifactRequest);
+            }
+            else
+            {
+                log.LogError($"The VM must be running to apply artifacts!  ResourceGroupName '{resourceGroupName}', LabName '{labName}', VM '{virtualMachineName}'");
+
+                return new ContentResult()
+                {
+                    Content = Content.GetHtmlResponse(Content.responseType.VirtualMachineNotRunning, false),
+                    ContentType = "text/html",
+                    StatusCode = 200
+                };
+            }
+
+            return new ContentResult()
+            {
+                Content = Content.GetHtmlResponse(Content.responseType.VirtualMachineSuccess, true),
+                ContentType = "text/html",
+                StatusCode = 200
+            };
+        }
+    }
+}
---
+++ samples/DevTestLabs/AzureFunctions/CSharp/Helpers/Settings.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Build.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctions
+{
+    public class Settings
+    {
+        // this is the .net core way to build configuration, includes both local debugging (local.settings.jon) 
+        // and running in Azure (Envrionment Variables set by app configuration)
+        // More information found here:  https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.2
+        private static IConfigurationRoot config = new ConfigurationBuilder()
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        private ILogger log { get; set; }
+
+        public Settings(ILogger functionsLog)
+        {
+            log = functionsLog;
+        }
+
+        public T GetSetting<T>(string SettingName)
+        {
+            // Get the application setting and validate it.  This is stored as an environment 
+            // variable in the App Service in Azure and in "local.settings.json" when local debugging
+            var settingValue = config.GetValue<T>(SettingName);
+
+            if (!CheckNullOrEmpty<T>(settingValue))
+            {
+                return settingValue;
+            }
+            else
+            {
+                log.LogError($"Missing [{SettingName}] in function application settings");
+                return default(T);
+            }
+        }
+
+        public static bool CheckNullOrEmpty<T>(T value)
+        {
+            if (typeof(T) == typeof(string))
+                return string.IsNullOrEmpty(value as string);
+
+            return value == null || value.Equals(default(T));
+        }
+    }
+}
