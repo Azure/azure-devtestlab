4e4d1e4aa4d50b5e03162835670e139129222a3f
Luca Bolognese <lucabol@microsoft.com>
2019-08-20 18:46:48 +0200

    FIX: make tests multi-thread safe (maybe)

--- samples/ClassroomLabs/Modules/Library/Tests/Fast-SharedGallery.test.ps1
+++ samples/ClassroomLabs/Modules/Library/Tests/Fast-SharedGallery.test.ps1
@@ -7,8 +7,23 @@ Import-Module $PSScriptRoot\..\Az.LabServices.psm1
 $la = Get-FastLabAccount
 $sg = Get-FastGallery
 
+$mtx = New-Object System.Threading.Mutex($false, "231db620-9fec-4a20-86dd-31812d055325")
+
+# This is an example where I can't think of a way to make the test multi-thread safe without using a mutex
 Describe 'Shared Gallery Management' {
-    It 'Can attach a shared library' {
+    It 'Can attach/detach a shared library' {
+
+        try {
+            if($mtx.WaitOne(60 * 60 * 1000)) {
+                # We have the mutex. Move on to main logic after the catch clause.
+            } else {
+                $false | Should -BeTrue -Because "Can't get the mutex before timing out"
+            }
+
+        } catch [System.Threading.AbandonedMutexException] {
+            # This means a previous test crashed before releasing the mutex, trying to bring the lab in the correct initial state
+            $la | Remove-AzLabAccountSharedGallery -SharedGalleryName $sg.Name
+        }
             
         $sg | Should -Not -Be $null
 
@@ -18,9 +33,7 @@ Describe 'Shared Gallery Management' {
         $imgs = $la | Get-AzLabAccountSharedImage
         $imgs.Count | Should -BeGreaterThan 0
 
-    }
-
-    It 'Can detach a shared library' {
-        $la | Remove-AzLabAccountSharedGallery -SharedGalleryName $sg.Name  
+        $la | Remove-AzLabAccountSharedGallery -SharedGalleryName $sg.Name
+        $mtx.ReleaseMutex()  
     }    
 }
