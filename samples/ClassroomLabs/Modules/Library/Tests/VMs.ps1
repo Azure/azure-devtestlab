d49ab29e49b1b83f0b33c8d322d08bbbf4dc4030
Luca Bolognese <lucabol@microsoft.com>
2019-08-27 14:02:02 +0200

    ADD: vms

---
+++ samples/ClassroomLabs/Modules/Library/Tests/VMs.ps1
@@ -0,0 +1,32 @@
+[cmdletbinding()]
+Param()
+Import-Module $PSScriptRoot\..\Az.LabServices.psm1
+
+. $PSScriptRoot\Utils.ps1
+
+
+Describe 'VMs' {
+
+    BeforeAll {
+        $script:lab = Get-FastLab -RandomName
+        $script:vms = $script:lab | Get-AzLabVm
+        $script:vm = $script:vms[0]
+    }
+
+    AfterAll {
+        $script:lab | Remove-AzLab
+    }
+    
+    It 'Can start/stop VM' {
+        $script:vm.Status | Should -Be 'Stopped'
+        $script:vm | Start-AzLabVm
+        $started = $script:lab | Get-AzLabVm -Status 'Running'
+        $started.Count | Should -Be 1
+        $started[0].Name | Should -Be $script:vm.Name
+    }
+
+    It 'Can stop VM' {
+        $script:vm | Stop-AzLabVm
+        $script:lab | Get-AzLabVm -Status 'Stopped' | Where-Object { $_.Name -eq $script:vm.Name} | Should -Not -BeNullOrEmpty
+    }
+}
