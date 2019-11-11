6b858f278c6a9035bdf556834303f860275d1a35
David Mikhayelyan <damikhay@microsoft.com>
2019-11-11 14:18:00 -0800

    Added first version of addUserToGroup artifact

---
+++ Artifacts/windows-add-user-to-group/addUserToGroup.ps1
@@ -0,0 +1,66 @@
+﻿#
+# Functions used in this script.
+#
+
+function Handle-LastError
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
+
+function Validate-Params
+{
+    [CmdletBinding()]
+    param(
+    )
+
+    if ([string]::IsNullOrEmpty($Username))
+    {
+        throw 'Username parameter is required.'
+    }
+    if ([string]::IsNullOrEmpty($Group))
+    {
+        throw 'Group parameter is required.'
+    }
+}
+
+
+###################################################################################################
+
+#
+# PowerShell configurations
+#
+
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
+
+###################################################################################################
+
+#
+# Main execution block.
+#
+
+try
+{
+    Validate-Params
+
+    Add-LocalGroupMember -Group $Group -Member $Username
+}
+catch
+{
+    Handle-LastError
+}
