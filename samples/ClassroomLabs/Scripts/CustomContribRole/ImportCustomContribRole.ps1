c95bafb11a8e6465211887fe6f83d8b423606547
Nicole Haugen <nicolela@microsoft.com>
2021-02-02 13:21:34 -0600

    updated script

--- samples/ClassroomLabs/Scripts/CustomContribRole/ImportCustomContribRole.ps1
+++ samples/ClassroomLabs/Scripts/CustomContribRole/ImportCustomContribRole.ps1
@@ -1,1 +1,8 @@
-New-AzRoleDefinition -InputFile ".\AzLabsCustomContribRole.json"
+$index = (Get-Content -path ".\AzLabsCustomContribRole.json" -Raw).IndexOf("{Your Sub Id}")
+if ($index -eq -1) {
+    New-AzRoleDefinition -InputFile ".\AzLabsCustomContribRole.json"
+}
+else {
+    Write-Error -Message "You must replace {Your Sub Id} in AzLabsCustomContribRole.json with your Azure subscription id."
+}
+
