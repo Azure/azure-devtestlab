bb7b5c145c47b9b6740ea4a8525549f510530d88
Luca Bolognese <lucabol@microsoft.com>
2021-02-16 18:22:08 +0100

    Removing LabCreator & csv files and adding timing to PublishAll.ps1

--- samples/ClassroomLabs/Modules/Library/Tools/Examples/PublishAll.ps1
+++ samples/ClassroomLabs/Modules/Library/Tools/Examples/PublishAll.ps1
@@ -9,7 +9,12 @@ param(
     $ThrottleLimit = 5
 )
 
+$outerScriptstartTime = Get-Date
+Write-Host "Executing Lab Creation Script, starting at $outerScriptstartTime" -ForegroundColor Green
+
 Import-Module ../../Az.LabServices.psm1 -Force
 Import-Module ../LabCreationLibrary.psm1 -Force
 
 $CsvConfigFile | Import-LabsCsv | Publish-Labs
+
+Write-Host "Completed running Bulk Lab Creation script, total duration $(((Get-Date) - $outerScriptstartTime).TotalMinutes) minutes" -ForegroundColor Green
