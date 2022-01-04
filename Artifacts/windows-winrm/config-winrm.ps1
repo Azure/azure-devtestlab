05a9fff946089e31d041c7c564764573f20cbc05
09setht <09setht@gmail.com>
2022-01-04 13:57:57 -0600

    add format specifier

--- Artifacts/windows-winrm/config-winrm.ps1
+++ Artifacts/windows-winrm/config-winrm.ps1
@@ -66,7 +66,7 @@ function New-Certificate
     # makecert ocassionally produces negative serial numbers, which golang tls/crypto < 1.6.1 cannot handle.
 	# https://github.com/golang/go/issues/8265
     $serial = Get-Random
-    $expiryDate = [datetime]::Now.AddYears(10).ToString()
+    $expiryDate = [datetime]::Now.AddYears(10).ToString("yyyy/MM/dd")
     .\makecert -r -pe -n CN=$HostName -b 01/01/2012 -e $expiryDate -eku 1.3.6.1.5.5.7.3.1 -ss my -sr localmachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12 -# $serial 2>&1 | Out-Null
 
     $thumbprint=(Get-ChildItem cert:\Localmachine\my | Where-Object { $_.Subject -eq "CN=" + $HostName } | Select-Object -Last 1).Thumbprint
