337d4c7f5009c2b65d9b677dcfaf7a21615eaf29
Roger Best <rbest@microsoft.com>
2022-04-06 16:26:01 -0700

    Add PublishAllWithOutput

---
+++ samples/ClassroomLabs/Modules/Library/BulkOperations/Examples/PublishAllWithOutput.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param(
+    [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+    [string]
+    $CsvConfigFile,
+
+    [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $CsvOutputFile,
+
+    [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+    [int]
+    $ThrottleLimit = 5
+)
+
+$outerScriptstartTime = Get-Date
+Write-Host "Executing Lab Creation Script, starting at $outerScriptstartTime" -ForegroundColor Green
+
+Import-Module ../../Az.LabServices.psm1 -Force
+Import-Module ../Az.LabServices.BulkOperations.psm1 -Force
+
+$labAccountResults = $CsvConfigFile | 
+        Import-LabsCsv |                                      # Import the CSV File into objects, including validation & transforms
+        New-AzLabAccountsBulk -ThrottleLimit $ThrottleLimit | # Create all the lab accounts
+        New-AzLabsBulk -ThrottleLimit $ThrottleLimit |        # Create all labs
+        Publish-AzLabsBulk -ThrottleLimit $ThrottleLimit      # Publish all the labs
+
+# Write out the results
+$labAccountResults | Export-LabsCsv -Path $CsvOutputFile
+
+Write-Host "Completed running Bulk Lab Creation script, total duration $(((Get-Date) - $outerScriptstartTime).TotalMinutes) minutes" -ForegroundColor Green
